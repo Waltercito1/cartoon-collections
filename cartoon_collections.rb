d3fcad54efca6adbca70360ae5f734036b968bc5
Walter Aab <aabwalter@yahoo.com>
2020-10-27 21:59:51 -0500

    commit tree dd71f13d6e1d1d789c18462d1025209f00c17171

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(roll_call_dwarves)
+  dwarves = []
+  roll_call_dwarves.each_with_index do |name, index|
+    dwarves << "#{index+1}. #{name}"
+  end
+  #binding.pry
+  puts dwarves
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|i| i.capitalize + '!'}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long_short)
+  long_short.any? do |call|
+    if call.length > 4 
+      return true
+    end
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  #cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |a| a == "cheddar" || a == "gouda" || a == "camembert" }
 end
